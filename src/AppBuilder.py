3cb70f5d5fc8305a98bbeb643c96af4fe68295af
cstlarry <larry@skillsource.org>
2020-08-25 23:54:54 -0700

    User comments added

--- src/AppBuilder.py
+++ src/AppBuilder.py
@@ -1,27 +1,33 @@
 from tkinter import *
-prompts = ["Wage"]
-fields = [None] * len(prompts)
+# Example of how to use the line below: prompts = ["Enter Wage", "Enter Hours Worked"]
+prompts = ["Enter Wage:", "Enter Hours Worked:"]
 
-
+# add you code to the run method below:
 def run():
-    text = fields[0].get()
-    num = float(text)
-    outputln(f'{num: .2f}')
+    wage = float(fields[0].get())
+    hours = float(fields[1].get())
+    pay = hours * wage
+    outputln(f'You earned ${pay: .2f}')
 
+# **************** Put helper methods below *********************
+
+# **************** DO NOT CHANGE ANYTHING BELOW THIS LINE **********************
 tkWindow = Tk()
 tkWindow.geometry('990x600')
 tkWindow.title('AppBuilder')
 
+fields = [None] * len(prompts)
 row_num = 0
 
 for prompt in prompts:
-    label = Label(tkWindow, text=prompt, justify=LEFT)
+    label = Label(tkWindow, text=prompt)
     label.grid(row=row_num, column=0)
     fields[row_num] = Entry(tkWindow, textvariable=f"{row_num}", width=100)
     fields[row_num].grid(row=row_num, column=1)
     row_num += 1
 
-runButton = Button(tkWindow, text="Run", command=run).grid(row=row_num, column=0)
+runButton = Button(tkWindow, text="Run", command=run)
+runButton.grid(row=row_num, column=1, ipadx=10)
 
 row_num += 1
 display = Text(tkWindow)
