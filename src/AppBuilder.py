3cd502ec344b0d20a06f99ec6b465cdc13c1b8cc
cstlarry <larry@skillsource.org>
2020-08-25 17:41:53 -0700

    Output fix

--- src/AppBuilder.py
+++ src/AppBuilder.py
@@ -1,34 +1,38 @@
 from tkinter import *
-prompts = ["Name"]
+prompts = ["Wage"]
 fields = [None] * len(prompts)
 
+
 def run():
-    outputln(fields[0].get())
+    text = fields[0].get()
+    num = float(text)
+    outputln(f'{num: .2f}')
 
 tkWindow = Tk()
-tkWindow.geometry('800x600')
+tkWindow.geometry('990x600')
 tkWindow.title('AppBuilder')
 
 row_num = 0
 
 for prompt in prompts:
-    label = Label(tkWindow, text=prompt).grid(row=row_num, column=0)
-    fields[row_num] = Entry(tkWindow, textvariable="field", width=100)
+    label = Label(tkWindow, text=prompt, justify=LEFT)
+    label.grid(row=row_num, column=0)
+    fields[row_num] = Entry(tkWindow, textvariable=f"{row_num}", width=100)
     fields[row_num].grid(row=row_num, column=1)
     row_num += 1
 
 runButton = Button(tkWindow, text="Run", command=run).grid(row=row_num, column=0)
 
-row_num +=  1
+row_num += 1
 display = Text(tkWindow)
-display.grid(row=row_num, column=1, rowspan=2)
+display.configure(font=("Courier", 12, "bold"))
+display.grid(row=row_num, column=1)
 
 def outputln(text_to_output):
-    display.insert(END, f'{text_to_output}\n', 'big')
+    display.insert(END, f'{text_to_output}\n')
 
 def output(text_to_output):
-    display.insert(END, f'{text_to_output}', 'big')
-
+    display.insert(END, f'{text_to_output}')
 
 tkWindow.mainloop()
 
